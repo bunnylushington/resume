e9c8b9a07dc4b4c1d70248531de5560f739d16bd
Kevin Montuori <montuori@gmail.com>
2013-08-13 12:32:37 -0400

    text header complete

--- lib/ResRender.pm
+++ lib/ResRender.pm
@@ -1,6 +1,42 @@
 package ResRender;
 
-use ResRender::Text;
+use strict;
+require Exporter;
+
+our @ISA = qw[ Exporter ];
+our @EXPORT_OK = qw[ show_work 
+                     centerline
+                     filepath 
+                     name 
+                     phone
+                     email
+                     addresses
+                  ];
+our %EXPORT_TAGS = ( all => \@EXPORT_OK );
+
+sub centerline {
+  my $text = shift;
+  ' ' x ((80 - length($text)) / 2) . $text;
+}
+
+sub show_work {
+  my $val = lc(shift);
+  ($val eq 'no' or $val eq 'false' or $val == 0) ? 0 : 1;
+}
+
+sub filepath {
+  my ($data, $ext, $output) = @_;
+  "$output/" . $data->{format_options}->{basename} . ".$ext";
+}
+
+sub name { shift->{name} }
+sub phone { shift->{telephone} }
+sub email { shift->{email} }
+sub addresses {
+  my $add = shift->{address};
+  (ref $add eq 'ARRAY') ? @{ $add } : ($add);
+}
+
 
 1;
 
--- lib/ResRender/Text.pm
+++ lib/ResRender/Text.pm
@@ -4,10 +4,21 @@ use strict;
 use autodie;
 use feature qw[ :5.10 ];
 
+use ResRender qw[ :all ];
+
 sub render {
   my (undef, $data, $output) = @_;
+  open my $fh, ">", filepath($data, 'txt', $output);
+  header($data, $fh);
+  close $fh;
 }
 
+sub header {
+  my ($data, $fh) = @_;
+  say $fh centerline(name($data));
+  say $fh centerline($_) for addresses($data);
+  say $fh centerline(join " --- ", email($data), phone($data));
+}
 
 1;
 
