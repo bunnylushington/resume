39b4f5bdb85de328a217e9a73a1c2158cb0f93ad
Kevin Montuori <montuori@gmail.com>
2013-08-13 13:07:59 -0400

    added head/prelude tex output

--- lib/ResRender/TeX.pm
+++ lib/ResRender/TeX.pm
@@ -1,11 +1,57 @@
 package ResRender::TeX;
 
 use strict;
+use autodie;
+use feature qw[ :5.10 ];
+
+use ResRender qw[ :all ];
+our $AUTOLOAD = ();
 
 sub render {
   my (undef, $data, $output) = @_;
+  open my $fh, ">", filepath($data, 'tex', $output);
+  preamble($fh);
+  header($data, $fh);
+  close $fh;
+}
+
+sub preamble {
+  my $fh = shift;
+  print $fh $_ for <DATA>
+}
+
+sub header {
+  my ($data, $fh) = @_;
+  something("this is text");
+  say $fh centerline(largeheadfont(name($data)));
+  say $fh centerline(smallheadfont($_)) for addresses($data);
+  say $fh centerline(smallheadfont(join " --- ", email($data), phone($data)));
+}
+  
+sub AUTOLOAD {
+  my $text = shift;
+  my $pkg = __PACKAGE__;
+  (my $tag = $AUTOLOAD) =~ s/${pkg}:://;
+  "\\${tag}{ $text }";
 }
 
 1;
 
-__END__
+__DATA__
+% -- format.
+\nopagenumbers
+\overfullrule= 0pt
+\nopagenumbers
+\parindent=0pt
+\vsize 9in
+\voffset=-0.2in
+
+% -- fonts.
+\font\smallheadfont=cmr10 at 10truept
+\font\largeheadfont=cmr10 at 20.74truept 
+\font\headingfont=cmr10 at 14truept
+
+% -- macros.
+\def\entry#1{\parindent 5mm\item{--}{#1}\parindent 0pt}
+\def\summary#1{\parindent 5mm\item{--}{#1}\parindent 0pt}
+
