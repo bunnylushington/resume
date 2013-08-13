a29a1170b4a233c2cfc997a5c41f884663964a7e
Kevin Montuori <montuori@gmail.com>
2013-08-13 13:19:50 -0400

    added postamble

--- lib/ResRender/HTML.pm
+++ lib/ResRender/HTML.pm
@@ -9,10 +9,25 @@ use ResRender qw[ :all ];
 sub render {
   my (undef, $data, $output) = @_;
   open my $fh, ">", filepath($data, 'html', $output);
+  preamble($data, $fh);
   header($data, $fh);
+  postamble($fh);
   close $fh;
 }
 
+sub preamble {
+  my ($data, $fh) = @_; 
+  say $fh "<html><head>";
+  say $fh "<title>", name($data), " -- Resume</title>";
+  print $fh $_ for <DATA>;
+  say $fh "</head><body>";
+} 
+
+sub postamble {
+  my $fh = shift;
+  say $fh "</body></html>";
+}
+  
 sub header {
   my ($data, $fh) = @_;
   say $fh div({class => 'name'}, name($data));
--- lib/ResRender/TeX.pm
+++ lib/ResRender/TeX.pm
@@ -12,6 +12,7 @@ sub render {
   open my $fh, ">", filepath($data, 'tex', $output);
   preamble($fh);
   header($data, $fh);
+  postamble($fh);
   close $fh;
 }
 
@@ -20,12 +21,18 @@ sub preamble {
   print $fh $_ for <DATA>
 }
 
+sub postamble {
+  my $fh = shift;
+  say $fh "\\vfill\\eject";
+}
+
 sub header {
   my ($data, $fh) = @_;
   something("this is text");
   say $fh centerline(largeheadfont(name($data)));
   say $fh centerline(smallheadfont($_)) for addresses($data);
   say $fh centerline(smallheadfont(join " --- ", email($data), phone($data)));
+  say "\\bigskip";
 }
   
 sub AUTOLOAD {
