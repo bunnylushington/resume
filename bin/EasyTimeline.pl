4c19ef7f077c24e123d7d17e8462dccb92a4e65b
Kevin Montuori <montuori@gmail.com>
2013-08-20 10:16:29 -0400

    jobs timeline looking ok when manually produced

--- bin/EasyTimeline.pl
+++ bin/EasyTimeline.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 # http://svn.wikimedia.org/viewvc/mediawiki/trunk/extensions/timeline
 
--- lib/ResRender/Graph.pm
+++ lib/ResRender/Graph.pm
@@ -2,91 +2,120 @@ package ResRender::Graph;
 
 use strict;
 use Data::Dumper;
+use Date::Calc;
 use ResRender qw[ :all ];
+use feature qw[ :5.10 ];
+
+
+my @colors = qw[ orange
+                 purple
+                 magenta
+                 redorange
+                 yellow2
+                 green
+                 darkblue
+                 lavender
+                 claret
+                 lightorange
+                 dullyellow
+                 kelleygreen
+                 blue
+                 lightpurple
+                 coral
+                 yelloworange
+                 teal
+                 oceanblue
+                 powderblue
+                 tan1
+                 pink
+                 drabgreen
+                 skyblue
+                 powderblue2
+                 tan2
+                 yellowgreen
+                 limegreen
+              ];
 
 sub render {
   my (undef, $data, $output) = @_;
   my $dates = make_date_hash($data);
-  print_header();
-  my @catagories = make_rows($data, $dates);
-  print_footer();
-  print_divs(@catagories);
-}
+  my $categories = make_categories($data);
+  for my $c (@{ $categories }) {
+    my $category = $c->[0];
+    my $itemlist = $c->[1];
+    my @items = map { $_->[0] } @{ $itemlist };
 
-sub print_divs {
-  for (@_) {
-    print qq!<div id="$_" style="height: 100%"></div>!;
+    say "PlotArea = left:150 bottom:60 top:0 right:50";
+    say "Alignbars = justify";
+    say "DateFormat = dd/mm/yyyy";
+    say "ImageSize = width:800 height:300";
+    say "Period = from:01/01/1995 till:01/01/2014";
+    say "TimeAxis = orientation:horizontal format:yyyy";
+    say "ScaleMajor = increment:2 start:1995";
+    say "Fonts = \n  id:sans font:OpenSans-Regular";
+    
+    say "Colors =";
+    for my $i (0 .. $#items) {
+      say " id:$i value:$colors[$i]";
+    }
+    say " id:job value:gray(0.8)";
+
+    say "BarData =";
+    for my $i (0 .. $#items) {
+      say qq! bar:$i text:"$items[$i]" !;
+    }
+    say qq!  bar:jobs !;
+
+    say "PlotData =";
+    my $pd = join " " => qw[ width:10
+                             textcolor:black
+                             align:left
+                             mark:(line,white)
+                             anchor:from
+                             shift:(10,-4) ];
+    say "  $pd";
+    my %ends = ();
+    for my $i (0 .. $#items) {
+      my ($skill, $keys) =  @{ $itemlist->[$i] };
+      for my $jobkey (@{ $keys }) {
+        my $from = $dates->{$jobkey}->{start};
+        my $till = $dates->{$jobkey}->{end};
+        $ends{$till}++;
+         say "  bar:$i from:$from till:$till color:$i";
+       }
+    }
+
+    my $legend_param = 'bar:jobs color:white textcolor:job';
+    for my $key (keys %{ $dates }) {
+      my $from = $dates->{$key}->{start};
+      my $till = $dates->{$key}->{end};
+      my $text = $dates->{$key}->{label};
+      say "   $legend_param from:$from till:$till text:$text";
+    }
+
+    last;
   }
-}
-
-sub print_header {
-  print <<"EoF";
-<script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization',
-       'version':'1','packages':['timeline']}]}"></script>
-<script type="text/javascript">
-  google.setOnLoadCallback(drawChart);
-
-function drawChart() {
-EoF
-}
-
-sub print_footer {
- print "}</script>";
+    
 }
 
 
-
-sub make_rows {
-  my ($data, $dates) = @_;
+sub make_categories {
+  my ($data) = shift;
   my @categories = ();
   my $tech = $data->{technology};
-  for my $category (@{ $tech }) {
-## --------------------------------------------------
-    my ($cat, $list) = each %{ $category };
-    push @categories => $cat;
-    print <<"EndOfJS";
-var container = document.getElementById('$cat');
-var chart = new google.visualization.Timeline(container);
-var t = new google.visualization.DataTable();
-
-var options = {
-  timeline: { showRowLabels: true, 
-              groupByRowLabel: true, 
-              colorByRowLabel: true  },
-  avoidOverlappingGridLines: true
-};
-
-  
-t.addColumn({ type: 'string', id: 'Technology' });
-t.addColumn({ type: 'string', id: 'Employer' });
-t.addColumn({ type: 'date', id: 'Start'});
-t.addColumn({ type: 'date', id: 'End' });
-  
-t.addRows([
-EndOfJS
-  ;
-
-    for my $item ( @{ $list } ) {
-      my ($label, $keys) = each %{ $item };
-      for my $key ( split /\s*,\s*/ => $keys ) {
-        my $start = $dates->{$key}->{start};
-        my $end = $dates->{$key}->{end};
-        my $co = $dates->{$key}->{label};
-        print qq![ '$label', '$co',  $start, $end ],\n!;
-      }
+  for my $c (@{ $tech }) {
+    my @category_data = ();
+    my ($category, $l) = each %{ $c };
+    for my $i (@{ $l }) {
+      my ($item, $key_string) = each %{ $i };
+      my @keys = split /\s*,\s*/ => $key_string;
+      push @category_data => [$item, \@keys];
     }
-## --------------------------------------------------
-    print <<"EndOfJS"
-
-]);
-chart.draw(t, options);
-EndOfJS
-;
-
+    push @categories => [$category, \@category_data];
   }
-  return @categories
+  return \@categories;
+  # [ [Languages, [ [Perl, [j1, j2, j3]], [Tcl, [j1, j2]] ]], [Infra ... ] ]
 }
-
 
 sub make_date_hash {
   my $data = shift;
@@ -103,18 +132,16 @@ sub make_date_hash {
 sub start_to_js {
   my $date = shift;
   my ($month, $year) = split /\s+/ => $date;
-  qq!new Date("$month 15, $year")!;
+  my ($y, $m, $d) = Date::Calc::Decode_Date_US2("$month 15, $year");
+  sprintf "%02d/%02d/%d" => $d, $m, $y;
 }
 
 sub end_to_js {
   my $date = shift;
-  if (lc $date eq 'present') {
-    qq!new Date()!;
-  }
-  else {
-    my ($month, $year) = split /\s+/ => $date;
-    qq!new Date("$month 15, $year")!;
-  }
+  my ($month, $year) = split /\s+/ => $date;
+  my ($y, $m, $d) = (lc $date eq 'present') 
+    ? Date::Calc::Today : Date::Calc::Decode_Date_US2("$month, 15, $year");
+  sprintf "%02d/%02d/%d" => $d, $m, $y;
 }
 
 
