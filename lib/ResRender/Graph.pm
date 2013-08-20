afcb89bdd6f4fb440369865bb917b084ac511fd7
Kevin Montuori <montuori@gmail.com>
2013-08-20 07:51:42 -0400

    working on easytimeline integration

--- lib/ResRender/Graph.pm
+++ lib/ResRender/Graph.pm
@@ -4,17 +4,68 @@ use strict;
 use Data::Dumper;
 use ResRender qw[ :all ];
 
-sub render_js {
+sub render {
   my (undef, $data, $output) = @_;
   my $dates = make_date_hash($data);
-  my $rows = make_rows($data, $dates);
+  print_header();
+  my @catagories = make_rows($data, $dates);
+  print_footer();
+  print_divs(@catagories);
 }
+
+sub print_divs {
+  for (@_) {
+    print qq!<div id="$_" style="height: 100%"></div>!;
+  }
+}
+
+sub print_header {
+  print <<"EoF";
+<script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization',
+       'version':'1','packages':['timeline']}]}"></script>
+<script type="text/javascript">
+  google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+EoF
+}
+
+sub print_footer {
+ print "}</script>";
+}
+
+
 
 sub make_rows {
   my ($data, $dates) = @_;
+  my @categories = ();
   my $tech = $data->{technology};
   for my $category (@{ $tech }) {
+## --------------------------------------------------
     my ($cat, $list) = each %{ $category };
+    push @categories => $cat;
+    print <<"EndOfJS";
+var container = document.getElementById('$cat');
+var chart = new google.visualization.Timeline(container);
+var t = new google.visualization.DataTable();
+
+var options = {
+  timeline: { showRowLabels: true, 
+              groupByRowLabel: true, 
+              colorByRowLabel: true  },
+  avoidOverlappingGridLines: true
+};
+
+  
+t.addColumn({ type: 'string', id: 'Technology' });
+t.addColumn({ type: 'string', id: 'Employer' });
+t.addColumn({ type: 'date', id: 'Start'});
+t.addColumn({ type: 'date', id: 'End' });
+  
+t.addRows([
+EndOfJS
+  ;
+
     for my $item ( @{ $list } ) {
       my ($label, $keys) = each %{ $item };
       for my $key ( split /\s*,\s*/ => $keys ) {
@@ -24,7 +75,16 @@ sub make_rows {
         print qq![ '$label', '$co',  $start, $end ],\n!;
       }
     }
+## --------------------------------------------------
+    print <<"EndOfJS"
+
+]);
+chart.draw(t, options);
+EndOfJS
+;
+
   }
+  return @categories
 }
 
 
