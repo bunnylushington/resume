3d7c2f4205e3f59ba9a26ad88b8f445e26559090
Kevin Montuori <montuori@gmail.com>
2013-08-24 09:45:50 -0400

    condensed into one graph

--- lib/ResRender/Graph.pm
+++ lib/ResRender/Graph.pm
@@ -6,6 +6,7 @@ use Date::Calc;
 use ResRender qw[ :all ];
 use feature qw[ :5.10 ];
 use FindBin;
+use Digest::MD5 qw[ md5_hex ];
 
 my @colors = qw[ orange
                  purple
@@ -38,71 +39,70 @@ my @colors = qw[ orange
 
 sub render {
   my (undef, $data, $output) = @_;
-  mkdir "$output/plots" unless -d "$output/plots";
   my $dates = make_date_hash($data);
   my $categories = make_categories($data);
-  my @plots = ();
+  mkdir "$output/plots" unless -d "$output/plots";
+  my $data_file = "$output/../work/skills";
+  open P, ">", $data_file;
+
+  say P "PlotArea = left:150 bottom:60 top:0 right:50";
+  say P "Alignbars = justify";
+  say P "DateFormat = dd/mm/yyyy";
+  say P "ImageSize = width:800 height:600";
+  say P "Period = from:01/01/1995 till:01/01/2014";
+  say P "TimeAxis = orientation:horizontal format:yyyy";
+  say P "ScaleMajor = increment:2 start:1995";
+  say P "Fonts = \n  id:sans font:OpenSans-Regular";
+
+  my $plot_spec = join " " => qw[ width:10 
+                                  textcolor:black 
+                                  align:left 
+                                  mark:(line,white) 
+                                  anchor:from
+                                  shift:(10,-4) ];
+
+  my ($color_idx, $bar_idx) = (0,1);
+  my (@color_data, @plot_data, @bar_data) = ();
   for my $c (@{ $categories }) {
-    my $category = $c->[0];
-    my $itemlist = $c->[1];
-    my @items = map { $_->[0] } @{ $itemlist };
-
-    open P, ">", "$output/../work/$category";
-    push @plots => $category;
-
-    say P "PlotArea = left:150 bottom:60 top:0 right:50";
-    say P "Alignbars = justify";
-    say P "DateFormat = dd/mm/yyyy";
-    say P "ImageSize = width:800 height:300";
-    say P "Period = from:01/01/1995 till:01/01/2014";
-    say P "TimeAxis = orientation:horizontal format:yyyy";
-    say P "ScaleMajor = increment:2 start:1995";
-    say P "Fonts = \n  id:sans font:OpenSans-Regular";
-    
-    say P "Colors =";
-    for my $i (0 .. $#items) {
-      say P " id:$i value:$colors[$i]";
-    }
-    say P " id:job value:gray(0.8)";
-
-    say P "BarData =";
-    for my $i (0 .. $#items) {
-      say P qq! bar:$i text:"$items[$i]" !;
-    }
-    say P qq!  bar:jobs !;
-
-    say P "PlotData =";
-    my $pd = join " " => qw[ width:10
-                             textcolor:black
-                             align:left
-                             mark:(line,white)
-                             anchor:from
-                             shift:(10,-4) ];
-    say P "  $pd";
-    my %ends = ();
-    for my $i (0 .. $#items) {
-      my ($skill, $keys) =  @{ $itemlist->[$i] };
-      for my $jobkey (@{ $keys }) {
+    my ($category, $itemlist) = @{ $c };
+    my $color = md5_hex($category);
+    push @color_data => 
+      sprintf "id:%s value:%s" => $color, $colors[$color_idx++];
+    for my $item (@{ $itemlist }) {
+      my ($skill, $jobkeys) = @{ $item };
+      push @bar_data => qq!bar:$bar_idx text:"$skill"!;
+      for my $jobkey (@{ $jobkeys }) {
         my $from = $dates->{$jobkey}->{start};
         my $till = $dates->{$jobkey}->{end};
-        $ends{$till}++;
-         say P "  bar:$i from:$from till:$till color:$i";
-       }
-    }
-
-    my $legend_param = 'bar:jobs color:white textcolor:job';
-    for my $key (keys %{ $dates }) {
-      my $from = $dates->{$key}->{start};
-      my $till = $dates->{$key}->{end};
-      my $text = $dates->{$key}->{label};
-      say P "   $legend_param from:$from till:$till text:$text";
+        push @plot_data => qq!bar:$bar_idx from:$from till:$till color:$color!;
+      }
+      $bar_idx++;
     }
   }
 
-  for my $plot (@plots) {
-    print `$FindBin::Bin/timeline $plot`;
+  say P "Colors=";
+  say P "  $_" for @color_data;
+  say P "  id:job value:gray(0.8)";
+
+  say P "BarData=";
+  say P "  $_" for @bar_data;
+  say P "  bar:jobs";
+
+  say P "PlotData=";
+  say P "  $plot_spec";
+  say P "  $_" for @plot_data;
+  
+  my $legend_param = 'bar:jobs color:white textcolor:job';
+  for my $key (keys %{ $dates }) {
+    my $from = $dates->{$key}->{start};
+    my $till = $dates->{$key}->{end};
+    my $text = $dates->{$key}->{label};
+    say P "  $legend_param from:$from till:$till text:$text";
   }
 
+  close P;
+  
+  print `$FindBin::Bin/timeline skills`;
 }
 
 
